fe3a97ff96c38057597885ce685450640db3fe22
Damian Peralta <damianperaltam@gmail.com>
2019-08-02 10:38:02 +0200

    Adding process.env.PORT

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var app = express();
 app.get("/users", (req, res, next) => {
   res.json(["Tony","Lisa","Michael","Ginger","Food"]);
  });
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
  console.log("Server running on port 3000");
 });
